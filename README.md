7df6d5324fe678fc209951adf4074229df828424
Derek Bekoe <debekoe@microsoft.com>
2018-03-12 14:32:36 -0700

    instance-1 (can watch files and print to console)

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # spot
+
+## Instances
+
+### instance-1
+
+A demo instance.
+
+```bash
+docker build instance-1 -t debekoe.azurecr.io/spot-instance-1:0.0.1
+
+docker run -d -p 5001:1000 -e PORT=1000 -e INSTANCE_TOKEN=XXXX debekoe.azurecr.io/spot-instance-1:0.0.1
+
+docker push debekoe.azurecr.io/spot-instance-1:0.0.1
+```
+
+
+```bash
+export SN=spot-instance-1-0003
+export RG=spot-instance-dev
+export LOC=westus
+export IMG=debekoe.azurecr.io/spot-instance-1:0.0.1
+az container create --resource-group $RG --name $SN --image $IMG --cpu 1 --memory 1 --registry-password $RPASS --ip-address public --ports 80 443 --dns-name-label $SN -e PORT=80 INSTANCE_TOKEN=debekoe C_EMAIL=derek@derekbekoe.com C_DOMAIN=$SN.$LOC.azurecontainer.io -l $LOC
+```
+
+**Version tracking**
+
+0.0.1 - Attempted certbot version on ACI but can't verify domain
+
+0.0.2 - Non-SSL version. Works with the console
+
+0.0.3 - Getting file watcher to work
---
+++ instance-1/app.js
@@ -0,0 +1,115 @@
+var fs = require('fs')
+var express = require('express');
+var os = require('os');
+var pty = require('node-pty');
+
+var app = express();
+var expressWs = require('express-ws')(app);
+
+var chokidar = require('chokidar');
+
+// One-liner for current directory, ignores .dotfiles
+chokidar.watch('/root', {ignored: /(^|[\/\\])\../, ignorePermissionErrors: true}).on('all', (event, path) => {
+  console.log(event, path);
+});
+
+var terminals = {};
+var logs = {};
+
+var instanceToken = process.env.INSTANCE_TOKEN;
+
+app.use('/build', express.static(__dirname + '/../build'));
+app.use('/.well-known', express.static('/.certbot/.well-known/'));
+
+var requiresValidToken = function (req, res, next) {
+  if (req.query.token == instanceToken) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+app.get('/health-check', requiresValidToken, (req, res) => res.sendStatus(200));
+
+app.get('/', requiresValidToken, function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/favicon.ico', function(req, res){
+  res.sendFile(__dirname + '/favicon.ico');
+});
+
+app.get('/style.css', function(req, res){
+  res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/main.js', requiresValidToken, function(req, res){
+  res.sendFile(__dirname + '/main.js');
+});
+
+app.post('/terminals', requiresValidToken, function (req, res) {
+  var cols = parseInt(req.query.cols),
+      rows = parseInt(req.query.rows),
+      term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+        name: 'xterm-color',
+        cols: cols || 80,
+        rows: rows || 24,
+        cwd: process.env.PWD,
+        env: process.env
+      });
+
+  console.log('Created terminal with PID: ' + term.pid);
+  terminals[term.pid] = term;
+  logs[term.pid] = '';
+  term.on('data', function(data) {
+    logs[term.pid] += data;
+  });
+  res.send(term.pid.toString());
+  res.end();
+});
+
+app.post('/terminals/:pid/size', requiresValidToken, function (req, res) {
+  var pid = parseInt(req.params.pid),
+      cols = parseInt(req.query.cols),
+      rows = parseInt(req.query.rows),
+      term = terminals[pid];
+
+  term.resize(cols, rows);
+  console.log('Resized terminal ' + pid + ' to ' + cols + ' cols and ' + rows + ' rows.');
+  res.end();
+});
+
+app.ws('/terminals/:pid', function (ws, req) {
+  if (req.query.token == instanceToken) {
+    var term = terminals[parseInt(req.params.pid)];
+    console.log('Connected to terminal ' + term.pid);
+    ws.send(logs[term.pid]);
+  
+    term.on('data', function(data) {
+      try {
+        ws.send(data);
+      } catch (ex) {
+        // The WebSocket is not open, ignore
+      }
+    });
+    ws.on('message', function(msg) {
+      term.write(msg);
+    });
+    ws.on('close', function () {
+      term.kill();
+      console.log('Closed terminal ' + term.pid);
+      // Clean things up
+      delete terminals[term.pid];
+      delete logs[term.pid];
+    });
+  } else {
+    ws.close();
+  }
+  
+});
+
+var port = process.env.PORT || 3000,
+    host = os.platform() === 'win32' ? '127.0.0.1' : '0.0.0.0';
+
+console.log('App listening to http://' + host + ':' + port);
+app.listen(port, host);
